d85d980e7b47f0354540023075bee8b1f8da4d1f
DovzhukTimofii <121349911+DovzhukTimofii@users.noreply.github.com>
2023-05-22 18:54:16 +0300

    0.3

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -9,7 +9,7 @@ export const Body = () => {
 
     const [data, setData] = useState({});
     const [dataFuture, setDataFuture] = useState({});
-    const [location, setLocation] = useState('')
+    const [location, setLocation] = useState('');
     const urlToday = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=7bbcd0641391e3867da7fe524a3ce421`;
     const urlFuture = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=7bbcd0641391e3867da7fe524a3ce421`;
 
@@ -55,7 +55,7 @@ export const Body = () => {
                         {data.main ? <span className='weather__temperature' >Real Feel{Math.round(data.main.feels_like)}ºC / {Math.round((data.main.feels_like * 9/5) + 32)}F</span> : null}
                         <span className='weather__hamidity'>Humidity {data.main ? <span>{data.main.humidity}</span> : null}%</span>
                     </div>
-                    <img className='weather__today-img' src={`icons/${data.weather && data.weather[0] ? data.weather[0].icon : '404'}.png`}></img>
+                    <img className='weather__today-img' src={`icons/${data.weather && data.weather[0] ? data.weather[0].icon : null}.png`}></img>
                 </div>
                 <div className='weather__future'>
                     <div className='weather__future-item'>
@@ -63,42 +63,42 @@ export const Body = () => {
                             <FirstDay/>
                             {dataFuture.list && dataFuture.list[0] ? <span className='weather__future-temperature'>{Math.round(dataFuture.list[0].main.temp)}ºC</span> : null}
                         </div>
-                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[0] ? dataFuture.list[0].weather[0].icon : '404'}.png`} alt="" />
+                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[0] ? dataFuture.list[0].weather[0].icon : null}.png`} alt="" />
                     </div>
                     <div className='weather__future-item'>
                         <div className='weather__future-description '>
                             <SecondDay/>
                             {dataFuture.list && dataFuture.list[1] ? <span className='weather__future-temperature'>{Math.round(dataFuture.list[1].main.temp)}ºC</span> : null}
                         </div>
-                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[1] ? dataFuture.list[1].weather[0].icon : '404'}.png`} alt="" />
+                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[1] ? dataFuture.list[1].weather[0].icon : null}.png`} alt="" />
                     </div>
                     <div className='weather__future-item'>
                         <div className='weather__future-description '>
                             <ThirdDay/>
                             {dataFuture.list && dataFuture.list[2] ? <span className='weather__future-temperature'>{Math.round(dataFuture.list[2].main.temp)}ºC</span> : null}
                         </div>
-                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[2] ? dataFuture.list[2].weather[0].icon : '404'}.png`} alt="" />
+                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[2] ? dataFuture.list[2].weather[0].icon : null}.png`} alt="" />
                     </div>
                     <div className='weather__future-item'>
                         <div className='weather__future-description '>
                             <FourthDay/>
                             {dataFuture.list && dataFuture.list[3] ? <span className='weather__future-temperature'>{Math.round(dataFuture.list[3].main.temp)}ºC</span> : null}
                         </div>
-                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[3] ? dataFuture.list[3].weather[0].icon : '404'}.png`} alt="" />
+                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[3] ? dataFuture.list[3].weather[0].icon : null}.png`} alt="" />
                     </div>
                     <div className='weather__future-item'>
                         <div className='weather__future-description '>
                             <FifthDay/>
                             {dataFuture.list && dataFuture.list[4] ? <span className='weather__future-temperature'>{Math.round(dataFuture.list[4].main.temp)}ºC</span> : null}
                         </div>
-                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[4] ? dataFuture.list[4].weather[0].icon : '404'}.png`} alt="" />
+                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[4] ? dataFuture.list[4].weather[0].icon : null}.png`} alt="" />
                     </div>
                     <div className='weather__future-item'>
                         <div className='weather__future-description '>
                             <SixthDay/>
                             {dataFuture.list && dataFuture.list[5] ? <span className='weather__future-temperature'>{Math.round(dataFuture.list[5].main.temp)}ºC</span> : null}
                         </div>
-                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[5] ? dataFuture.list[5].weather[0].icon : '404'}.png`} alt="" />
+                        <img className='weather__future-img' src={`icons/${dataFuture.list && dataFuture.list[5] ? dataFuture.list[5].weather[0].icon : null}.png`} alt="" />
                     </div>
                 </div>
             </main>
